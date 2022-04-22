18735832e7ccf597d485c2f3c47872666c458c4c
haifuns <haifuns@163.com>
2022-04-22 19:16:52 +0800

    插入排序

--- stage_01/src/Code03_InsertSort.java
+++ stage_01/src/Code03_InsertSort.java
@@ -6,6 +6,19 @@
  */
 public class Code03_InsertSort {
 
+    public static void insertSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            for (int j = i - 1; arr[j] > arr[j + 1] && j >= 0; j--) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+
     public static void swap(int[] arr, int i, int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
@@ -22,7 +35,7 @@ public class Code03_InsertSort {
     public static void main(String[] arg) {
         int[] arr = new int[] { 1, 2, 5, 1, 7, 3, 2, 8 };
         print(arr);
-        // bubbleSort(arr);
+        insertSort(arr);
         print(arr);
     }
 }
