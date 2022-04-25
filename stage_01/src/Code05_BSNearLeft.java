1b538a5cd16531c4829adc7f686287dfc0543e68
haifuns <haifuns@163.com>
2022-04-25 19:56:50 +0800

    二分法 - 在有序数组中找到大于等于num最左的位置

--- stage_01/src/Code05_BSNearLeft.java
+++ stage_01/src/Code05_BSNearLeft.java
@@ -1,33 +1,33 @@
 import java.util.Arrays;
 
 /**
- * 二分法 - 在有序数组中找到num
+ * 二分法 - 在有序数组中找到大于等于num最左的位置
  * 
  * @author haifuns
- * @date 2022-04-25 19:13
+ * @date 2022-04-25 19:43
  */
-public class Code04_BSExist {
+public class Code05_BSNearLeft {
 
-    public static boolean find(int[] arr, int num) {
+    public static int mostLeftNoLessNumIndex(int[] arr, int num) {
         if (arr == null || arr.length == 0) {
-            return false;
+            return -1;
         }
 
-        int left = 0;
-        int right = arr.length - 1;
+        int l = 0;
+        int r = arr.length - 1;
+        int ans = -1;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] == num) {
-                return true;
-            } else if (arr[mid] < num) {
-                left = mid + 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (arr[mid] >= num) {
+                ans = mid;
+                r = mid - 1;
             } else {
-                right = mid - 1;
+                l = mid + 1;
             }
         }
 
-        return false;
+        return ans;
     }
 
     public static int[] generateRandomArray(int maxSize, int maxValue) {
@@ -38,13 +38,13 @@ public class Code04_BSExist {
         return arr;
     }
 
-    public static boolean test(int[] arr, int num) {
+    public static int test(int[] arr, int num) {
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == num) {
-                return true;
+            if (arr[i] >= num) {
+                return i;
             }
         }
-        return false;
+        return -1;
     }
 
     public static void main(String[] args) {
@@ -56,7 +56,7 @@ public class Code04_BSExist {
             int[] arr = generateRandomArray(maxSize, maxValue);
             Arrays.sort(arr);
             int value = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
-            if (test(arr, value) != find(arr, value)) {
+            if (test(arr, value) != mostLeftNoLessNumIndex(arr, value)) {
                 succeed = false;
                 break;
             }
