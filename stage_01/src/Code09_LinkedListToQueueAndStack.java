1e0fa40344979b902f84a6271ba3794b779f7d83
haifuns <haifuns@163.com>
2022-05-06 12:54:51 +0800

    单链表实现队列和栈

---
+++ stage_01/src/Code09_LinkedListToQueueAndStack.java
@@ -0,0 +1,222 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * 单链表实现队列和栈
+ * 
+ * @author haifuns
+ * @date 2022-04-28 19:41
+ */
+public class Code09_LinkedListToQueueAndStack {
+
+    public static class Node<V> {
+
+        private V value;
+
+        private Node<V> next;
+
+        public Node(V v) {
+            this.value = v;
+            next = null;
+        }
+
+    }
+
+    public static class MyQueue<V> {
+
+        private Node<V> head;
+
+        private Node<V> tail;
+
+        private int size;
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public int size() {
+            return this.size;
+        }
+
+        public void offer(V v) {
+
+            Node<V> cur = new Node<V>(v);
+            if (tail == null) {
+                head = cur;
+                tail = cur;
+            } else {
+                tail.next = cur;
+                tail = cur;
+            }
+
+            size++;
+        }
+
+        public V poll() {
+            V ans = null;
+            if (head != null) {
+                ans = head.value;
+                head = head.next;
+                size--;
+            }
+
+            if (head == null) {
+                tail = null;
+            }
+            return ans;
+        }
+
+        public V peek() {
+            V ans = null;
+            if (head != null) {
+                ans = head.value;
+            }
+            return ans;
+        }
+    }
+
+    public static class MyStack<V> {
+
+        private Node<V> head;
+
+        private int size;
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public int size() {
+            return this.size;
+        }
+
+        public void push(V v) {
+            Node<V> cur = new Node<V>(v);
+            if (head == null) {
+                head = cur;
+            } else {
+                cur.next = head;
+                head = cur;
+            }
+            size++;
+        }
+
+        public V pop() {
+            V ans = null;
+            if (head != null) {
+                ans = head.value;
+                head = head.next;
+                size--;
+            }
+            return ans;
+        }
+
+        public V peek() {
+            return head != null ? head.value : null;
+        }
+
+    }
+
+    public static void testQueue() {
+        MyQueue<Integer> myQueue = new MyQueue<>();
+        Queue<Integer> test = new LinkedList<>();
+        int testTime = 5000000;
+        int maxValue = 200000000;
+        System.out.println("测试开始！");
+        for (int i = 0; i < testTime; i++) {
+            if (myQueue.isEmpty() != test.isEmpty()) {
+                System.out.println("Oops!");
+            }
+            if (myQueue.size() != test.size()) {
+                System.out.println("Oops!");
+            }
+            double decide = Math.random();
+            if (decide < 0.33) {
+                int num = (int) (Math.random() * maxValue);
+                myQueue.offer(num);
+                test.offer(num);
+            } else if (decide < 0.66) {
+                if (!myQueue.isEmpty()) {
+                    int num1 = myQueue.poll();
+                    int num2 = test.poll();
+                    if (num1 != num2) {
+                        System.out.println("Oops!");
+                    }
+                }
+            } else {
+                if (!myQueue.isEmpty()) {
+                    int num1 = myQueue.peek();
+                    int num2 = test.peek();
+                    if (num1 != num2) {
+                        System.out.println("Oops!");
+                    }
+                }
+            }
+        }
+        if (myQueue.size() != test.size()) {
+            System.out.println("Oops!");
+        }
+        while (!myQueue.isEmpty()) {
+            int num1 = myQueue.poll();
+            int num2 = test.poll();
+            if (num1 != num2) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("测试结束！");
+    }
+
+    public static void testStack() {
+        MyStack<Integer> myStack = new MyStack<>();
+        Stack<Integer> test = new Stack<>();
+        int testTime = 5000000;
+        int maxValue = 200000000;
+        System.out.println("测试开始！");
+        for (int i = 0; i < testTime; i++) {
+            if (myStack.isEmpty() != test.isEmpty()) {
+                System.out.println("Oops!");
+            }
+            if (myStack.size() != test.size()) {
+                System.out.println("Oops!");
+            }
+            double decide = Math.random();
+            if (decide < 0.33) {
+                int num = (int) (Math.random() * maxValue);
+                myStack.push(num);
+                test.push(num);
+            } else if (decide < 0.66) {
+                if (!myStack.isEmpty()) {
+                    int num1 = myStack.pop();
+                    int num2 = test.pop();
+                    if (num1 != num2) {
+                        System.out.println("Oops!");
+                    }
+                }
+            } else {
+                if (!myStack.isEmpty()) {
+                    int num1 = myStack.peek();
+                    int num2 = test.peek();
+                    if (num1 != num2) {
+                        System.out.println("Oops!");
+                    }
+                }
+            }
+        }
+        if (myStack.size() != test.size()) {
+            System.out.println("Oops!");
+        }
+        while (!myStack.isEmpty()) {
+            int num1 = myStack.pop();
+            int num2 = test.pop();
+            if (num1 != num2) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("测试结束！");
+    }
+
+    public static void main(String[] args) {
+        testQueue();
+        testStack();
+    }
+}
