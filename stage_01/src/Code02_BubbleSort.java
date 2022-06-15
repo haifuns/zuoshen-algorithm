1563a0c05d482c8a3c105387a2c291d526c2aa1a
haifuns <haifuns@163.com>
2022-06-15 10:40:00 +0800

    冒泡排序提前退出优化

--- stage_01/src/Code02_BubbleSort.java
+++ stage_01/src/Code02_BubbleSort.java
@@ -16,13 +16,24 @@ public class Code02_BubbleSort {
         // 0 - n-1
         // 0 - n-2
         for (int end = n - 1; end >= 0; end--) {
+
+            // 提前退出冒泡循环的标志位
+            boolean flag = false;
+
             // 0, 1
             // 1, 2
             // 2, 3
             for (int second = 1; second <= end; second++) {
                 if (arr[second - 1] > arr[second]) {
+                    // 表示有数据交换
+                    flag = true;
                     swap(arr, second - 1, second);
                 }
+            }
+
+            // 没有数据交换，提前退出
+            if (!flag) {
+                break;
             }
         }
     }
